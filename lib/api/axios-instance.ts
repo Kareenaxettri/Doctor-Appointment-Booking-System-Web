3ceae8a495b22a312922326c27061d4b822a1ee6
Kareena Tandan <kareenatandan477@gmail.com>
2026-07-28 23:10:36 +0545

    Add lib/api/axios-instance.ts

--- lib/api/axios-instance.ts
+++ lib/api/axios-instance.ts
@@ -1,13 +1,25 @@
 import axios from "axios";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
-    || "http://localhost:8089";
+  || "http://localhost:8089";
 
 const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: BASE_URL,
+  timeout: 15000,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      if (typeof window !== "undefined") {
+        document.cookie = "auth_token=; path=/; max-age=0";
+        document.cookie = "user_data=; path=/; max-age=0";
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
